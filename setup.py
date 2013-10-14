db0a2499c64783f05b5c16db319ce9fed2cb030c
Martin Atkins <mart@degeneration.co.uk>
2013-10-13 17:23:37 -0700

    v0.0.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="datafork",
-    version="dev",
+    version="0.0.2",
     description="Forkable global state",
     packages=['datafork'],
     author="Martin Atkins",
