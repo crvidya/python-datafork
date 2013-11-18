3ad26cffcbdc32efe7f67f3ed426c5ed70abf34b
Martin Atkins <mart@degeneration.co.uk>
2013-11-17 16:19:27 -0800

    Set version number for 0.0.6 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="datafork",
-    version="dev",
+    version="0.0.6",
     description="Forkable global state",
     packages=['datafork'],
     author="Martin Atkins",
